8a76f56f331b18fa6f540927098994445c7e1cb3
Юрий Кузнецов <omega131995@outlook.com>
2024-09-29 21:08:56 +0800

    Отправка формы

--- src/widgets/registration-form/index.tsx
+++ src/widgets/registration-form/index.tsx
@@ -1,12 +1,14 @@
 import {Button, ButtonBackground, Flex, Form, Input, Orientation} from "@mistek/freedom-ui";
 import * as styles from "./styles.module.css";
 import {EnvelopeAt, Key, Person} from "react-bootstrap-icons";
+import {observer} from "mobx-react-lite";
+import {sendRegistrationForm} from "../../features/registration-form/registrationFormState";
 
-export const RegistrationForm = () => {
+export const RegistrationForm = observer(() => {
     const iconSize = 25;
 
     return <Flex orientation={Orientation.vertical} className={styles.container}>
-        <Form handleSubmit={(form) => console.log(form)}>
+        <Form handleSubmit={sendRegistrationForm}>
             <Input name="login" placeholder="Введите логин" iconLeft={<Person width={iconSize} height={iconSize}/>}/>
             <Input name="email" placeholder="Введите почту" iconLeft={<EnvelopeAt width={iconSize} height={iconSize}/>}/>
             <Input type="password" name="password" placeholder="Введите пароль" iconLeft={<Key width={iconSize} height={iconSize}/>}/>
@@ -14,4 +16,4 @@ export const RegistrationForm = () => {
             <Button type="submit" bg={ButtonBackground.success}>Создать аккаунт</Button>
         </Form>
     </Flex>
-}
+})
