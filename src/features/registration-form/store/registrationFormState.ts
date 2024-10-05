4fe4756556eea5fd90ee4bf5d390b183b63a2b49
Юрий Кузнецов <omega131995@outlook.com>
2024-10-05 23:18:41 +0800

    Обертка над grpc клиентом в форме регистрации

--- src/features/registration-form/store/registrationFormState.ts
+++ src/features/registration-form/store/registrationFormState.ts
@@ -3,6 +3,7 @@ import {AuthServiceClient} from "../../../shared/grpcClients/auth/AuthServiceSer
 import {RegisterRequest} from "../../../shared/grpcClients/auth/AuthService_pb";
 import {IToast} from "@mistek/freedom-ui";
 import {grpcErrorHandler} from "../../../shared/grpcClients/grpcErrorHandler";
+import {ServerResponse} from "../../../entities/ServerResponse";
 
 const sendRegistrationForm = action((form:any, addToast: (toast: IToast) => void) => {
     const client = new AuthServiceClient("http://localhost:8080/")
@@ -22,4 +23,30 @@ const sendRegistrationForm = action((form:any, addToast: (toast: IToast) => void
     })
 })
 
-export {sendRegistrationForm}
+const sendRegistrationFormNew = action(async (form:any) : Promise<ServerResponse> => {
+    const client = new AuthServiceClient("http://localhost:8080/")
+    const request = new RegisterRequest();
+    request.setLogin(form.login);
+    request.setEmail(form.email);
+    request.setPassword(form.password);
+    request.setToken("Token");
+
+    try{
+        const response = await new Promise<ServerResponse>((resolve, reject) => {
+            client.register(request, {}, (err, response) => {
+
+                if(err){
+                    reject({isSuccess: false, errorMessage: err.message} as ServerResponse);
+                } else {
+                    resolve({isSuccess: true, errorMessage: ""} as ServerResponse);
+                }
+            })
+        })
+
+        return response;
+    }catch (error){
+        return error as ServerResponse;
+    }
+})
+
+export {sendRegistrationForm, sendRegistrationFormNew}
--- src/features/registration-form/ui/index.tsx
+++ src/features/registration-form/ui/index.tsx
@@ -1,15 +1,23 @@
-import {Button, ButtonBackground, Flex, Form, Input, Orientation, useToast} from "@mistek/freedom-ui";
+import {Button, ButtonBackground, Flex, Form, Input, Orientation, ToastBackground, useToast} from "@mistek/freedom-ui";
 import * as styles from "../style/styles.module.scss";
 import {EnvelopeAt, Key, Person} from "react-bootstrap-icons";
 import {observer} from "mobx-react-lite";
-import {sendRegistrationForm} from "../store/registrationFormState";
+import {sendRegistrationFormNew} from "../store/registrationFormState";
 
 export const RegistrationForm = observer(() => {
     const {addToast} = useToast();
     const iconSize = 25;
 
+    const sendForm = async (form: any) => {
+        await sendRegistrationFormNew(form).then(response => {
+            if(!response.isSuccess){
+                addToast({label: "Ошибка", description: response.errorMessage, bg: ToastBackground.danger})
+            }
+        })
+    }
+
     return <Flex orientation={Orientation.vertical} className={styles.container}>
-        <Form handleSubmit={(form) => sendRegistrationForm(form, addToast)}>
+        <Form handleSubmit={sendForm}>
             <Input name="login" placeholder="Введите логин" iconLeft={<Person width={iconSize} height={iconSize}/>}/>
             <Input name="email" placeholder="Введите почту" iconLeft={<EnvelopeAt width={iconSize} height={iconSize}/>}/>
             <Input type="password" name="password" placeholder="Введите пароль" iconLeft={<Key width={iconSize} height={iconSize}/>}/>
