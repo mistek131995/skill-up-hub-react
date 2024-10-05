5b4ad1e5c7b27c649ce3db3e4d6edf35a4f8c468
Юрий Кузнецов <omega131995@outlook.com>
2024-10-05 22:38:03 +0800

    Обновлен grpc клиент, обработчик ошибок grpc клиента

--- src/features/login-form/store/loginFormState.ts
+++ src/features/login-form/store/loginFormState.ts
@@ -1,26 +1,24 @@
 import {action} from "mobx";
-import {IToast, ToastBackground} from "@mistek/freedom-ui";
-import {AuthServiceClient} from "../../../app/grpc/AuthServiceServiceClientPb";
-import {LoginRequest} from "../../../app/grpc/AuthService_pb";
+import {IToast} from "@mistek/freedom-ui";
+import {AuthServiceClient} from "../../../shared/grpcClients/auth/AuthServiceServiceClientPb";
+import {LoginRequest} from "../../../shared/grpcClients/auth/AuthService_pb";
 import {generateFingerprint} from "../../../shared/utlis/generateFingerprint";
+import {grpcErrorHandler} from "../../../shared/grpcClients/grpcErrorHandler";
 
 const sendLoginForm = action(async (form: any, addToast: (toast: IToast) => void) => {
-    const client = new AuthServiceClient("http://localhost:5180/");
+    const client = new AuthServiceClient("http://localhost:8080/");
     const request = new LoginRequest();
     request.setLogin(form.login);
     request.setPassword(form.password);
     request.setToken("Token");
 
-    client.login(request, {        "Fingerprint": await generateFingerprint()
+    client.login(request, {
+        "Fingerprint": await generateFingerprint()
     }, (err, response) => {
         if(err){
-            addToast({
-                label: "Ошибка",
-                description: err.message,
-                bg: ToastBackground.danger
-            })
+            grpcErrorHandler(err, addToast)
         } else {
-            console.log(response);
+            localStorage.setItem("token", response.getAccesstoken())
         }
     })
 })
--- src/features/registration-form/store/registrationFormState.ts
+++ src/features/registration-form/store/registrationFormState.ts
@@ -1,10 +1,11 @@
 import {action} from "mobx";
-import {AuthServiceClient} from "../../../app/grpc/AuthServiceServiceClientPb";
-import {RegisterRequest} from "../../../app/grpc/AuthService_pb";
-import {IToast, ToastBackground} from "@mistek/freedom-ui";
+import {AuthServiceClient} from "../../../shared/grpcClients/auth/AuthServiceServiceClientPb";
+import {RegisterRequest} from "../../../shared/grpcClients/auth/AuthService_pb";
+import {IToast} from "@mistek/freedom-ui";
+import {grpcErrorHandler} from "../../../shared/grpcClients/grpcErrorHandler";
 
 const sendRegistrationForm = action((form:any, addToast: (toast: IToast) => void) => {
-    const client = new AuthServiceClient("http://localhost:5180/")
+    const client = new AuthServiceClient("http://localhost:8080/")
     const request = new RegisterRequest();
     request.setLogin(form.login);
     request.setEmail(form.email);
@@ -14,11 +15,7 @@ const sendRegistrationForm = action((form:any, addToast: (toast: IToast) => void
     client.register(request, {}, (err, response) => {
 
         if(err){
-            addToast({
-                label: "Ошибка",
-                description: err.message,
-                bg: ToastBackground.danger
-            })
+            grpcErrorHandler(err, addToast)
         } else {
             console.log(response);
         }
